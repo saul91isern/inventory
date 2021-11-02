26c1129f661bf6dcf08f3feb552d60bef3f85fcb
saul.isern <saul.isern@stord.com>
2021-11-02 19:48:00 +0100

    fix: tenant_id should be part of the primary key

--- priv/repo/migrations/20211102134019_create_companies.exs
+++ priv/repo/migrations/20211102134019_create_companies.exs
@@ -5,7 +5,7 @@ defmodule Inventory.Repo.Migrations.CreateCompanies do
     create table(:companies, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :name, :string, null: false
-      add :tenant_id, :uuid
+      add :tenant_id, :uuid, primary_key: true
 
       timestamps()
     end
